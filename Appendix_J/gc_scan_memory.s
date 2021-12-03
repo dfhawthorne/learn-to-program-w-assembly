b7d77bdf3b4c497a2a8db377ae9d6434040831cb
Douglas Hawthorne <dfhawthorne@computer.org>
2021-12-04 07:45:01 +1100

    My implementation of Appendix_J/gc_scan_memory.s

--- Appendix_J/gc_scan_memory.s
+++ Appendix_J/gc_scan_memory.s
@@ -8,6 +8,40 @@ gc_scan_memory:
     # it for potential pointers to the heap.
     # %rdi points to memory segment
     # %rsi is the size
-    enter   $0, $0
+    enter   $32, $0
+    pushq   $0
+    movq    %rcx, -8(%rbp)
+    movq    %rax, -16(%rbp)
+    movq    %rsi, -24(%rbp)
+    # Scan through body of segment looking for possible pointers.
+    movq    $0, %rcx                    # Initialise index
+    shrq    $3, %rsi                    # Divide by 8 to get # quad words
+loop:
+    leaq    HEADER_SIZE(%rdi), %rax
+    movq    0(%rax, %rcx, 8), %rax
+
+    # If a pointer is found, check that it points into the heap.
+    cmpq    $0, %rax
+    je      next
+    cmpq    %rax, heap_start
+    ja      next
+    cmpq    %rax, heap_end
+    jb      next
+    
+    # Mark the referenced segment as in use
+    subq    $HEADER_SIZE, %rax
+    movq    $1, HDR_IN_USE_OFFSET(%rax)
+    
+    # Look at next slot
+next:
+    incq    %rcx
+    cmpq    %rsi, %rcx
+    jb      loop
+
+    # Return to caller after restoring registers
+goback:
+    movq    -24(%rbp), %rsi
+    movq    -16(%rbp), %rax
+    movq    -8(%rbp), %rcx
     leave
     ret
